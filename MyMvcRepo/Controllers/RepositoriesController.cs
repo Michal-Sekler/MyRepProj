919881665e090ecacb40e6412d751c80a3f12b7f
Michal Sekler <michalw7740@gmail.com>
2019-11-06 12:09:26 +0200

    initial commit

---
+++ MyMvcRepo/Controllers/RepositoriesController.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMvcRepo.Controllers
+{
+    public class RepositoriesController : Controller
+    {
+        
+        // GET: Repositories
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        //save in session the the repository's names the user searched.
+        public string addSearchToSession(string searchName)
+        {
+            var searchObjects = (Session["search"] as List<string>) ?? new List<string>();
+            searchObjects.Add(searchName);
+            Session["search"] = searchObjects;
+            return JsonConvert.SerializeObject(searchObjects.ToArray());
+        }
+
+
+
+
+    }
+}
---
+++ MyMvcRepo/Models/Repository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMvcRepo.Models
+{
+    public class Repository
+    {
+        public string Name { get; set; }
+        public string HtmlUrl { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
